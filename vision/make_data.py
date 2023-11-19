1e1e6cc6dba073eb8be268876d7bac52daa391a4
brettcaswell <caswellbrett@gmail.com>
2023-11-19 11:55:33 -0800

    complete first iteration of make_data

--- vision/make_data.py
+++ vision/make_data.py
@@ -8,28 +8,32 @@ sign_list = list(string.ascii_uppercase) + list(range(10))
 dataset_size = 100
 
 cap = cv2.VideoCapture(0)
-for j in range(sign_list):
-    if not os.path.exists(os.path.join(DATA_PATH, str(j))):
-        os.makedirs(os.path.join(DATA_PATH, str(j)))
+for j in range(len(sign_list)):
+    if not os.path.exists(os.path.join(DATA_PATH, str(sign_list[j]))):
+        os.makedirs(os.path.join(DATA_PATH, str(sign_list[j])))
 
-    print('Collecting data for class {}'.format(j))
+    print('Collecting data for class {}'.format(sign_list[j]))
 
     while True:
         ret, frame = cap.read()
-        cv2.putText(frame, 'Ready? Press "Q" ! :)', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3,
+        cv2.putText(frame, 'Ready? Press "' +
+                    str(sign_list[j]) +
+                    '" ! :)', (100, 50), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3,
                     cv2.LINE_AA)
         cv2.imshow('frame', frame)
-        if cv2.waitKey(25) == ord('q'):
+        if cv2.waitKey(25) == ord(str(sign_list[j]).lower()):
             break
 
-    counter = 0
-    while counter < dataset_size:
+    for i in range(dataset_size):
         ret, frame = cap.read()
         cv2.imshow('frame', frame)
         cv2.waitKey(25)
-        cv2.imwrite(os.path.join(DATA_PATH, str(j), '{}.jpg'.format(counter)), frame)
+        cv2.imwrite(os.path.join(DATA_PATH, 
+                                 str(sign_list[j]), 
+                                 '{}.jpg'.format(i)), 
+                                 frame)
 
-        counter += 1
 
 cap.release()
 cv2.destroyAllWindows()
