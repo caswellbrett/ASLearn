d207f90fafd8808c5610a38815669fdb2bb85408
brettcaswell <caswellbrett@gmail.com>
2023-11-19 10:08:48 -0800

    start on make data file

---
+++ vision/make_data.py
@@ -0,0 +1,35 @@
+import os
+import cv2
+import string
+
+DATA_PATH = './data'
+
+sign_list = list(string.ascii_uppercase) + list(range(10))
+dataset_size = 100
+
+cap = cv2.VideoCapture(0)
+for j in range(sign_list):
+    if not os.path.exists(os.path.join(DATA_PATH, str(j))):
+        os.makedirs(os.path.join(DATA_PATH, str(j)))
+
+    print('Collecting data for class {}'.format(j))
+
+    while True:
+        ret, frame = cap.read()
+        cv2.putText(frame, 'Ready? Press "Q" ! :)', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3,
+                    cv2.LINE_AA)
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(25) == ord('q'):
+            break
+
+    counter = 0
+    while counter < dataset_size:
+        ret, frame = cap.read()
+        cv2.imshow('frame', frame)
+        cv2.waitKey(25)
+        cv2.imwrite(os.path.join(DATA_PATH, str(j), '{}.jpg'.format(counter)), frame)
+
+        counter += 1
+
+cap.release()
+cv2.destroyAllWindows()
